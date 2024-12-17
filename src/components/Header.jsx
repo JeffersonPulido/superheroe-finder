c092c46d4c7dc671ed70292f5df53d9b72d27bbf
Jefferson Pulido <jeffersonpulido55@gmail.com>
2024-12-17 09:21:25 -0500

    [FEAT] Fix styles and bgColor dark

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,3 @@
 export default function Header() {
-    return <h1 className="py-4 text-4xl font-bold text-center">SuperHero Finder</h1>
+    return <h1 className="py-4 text-4xl font-bold text-center text-white">SuperHero Finder</h1>
 }
